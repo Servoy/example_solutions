7825253c2f6362ae1aff3dbf2f584b8da77b335a
Jan Blok <jblok@servoy.com>
2014-10-17 17:25:08 +0200

    SVY-7368 move servoycomponents

---
+++ basic_sample/forms/companies.js
@@ -0,0 +1,27 @@
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"E62CD739-B0A7-40DD-8B84-57E4BFE4E6C7"}
+ */
+function onPreviousAction(event) 
+{
+	foundset.setSelectedIndex(foundset.getSelectedIndex()-1);
+}
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"BB65631B-13B1-447F-9DD7-7E9C140469BA"}
+ */
+function onNextAction(event) {
+	foundset.setSelectedIndex(foundset.getSelectedIndex()+1);
+}
